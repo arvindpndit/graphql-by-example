e99565fdb582181c9c9b95f89ef2aeef1d2f3d87
arvindpndit <arvindpandit9808@gmail.com>
2024-04-20 12:23:03 +0530

    fetch the posts on the frontend

--- graphql-client/src/App.jsx
+++ graphql-client/src/App.jsx
@@ -3,7 +3,9 @@ import { useQuery, gql } from "@apollo/client";
 
 const GET_DATA = gql`
   query {
-    say(name: "arvind")
+    posts {
+      title
+    }
   }
 `;
 
@@ -13,7 +15,14 @@ function App() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error --- </p>;
 
-  return <>{data?.say}</>;
+  return (
+    <>
+      <h2>All Posts</h2>
+      {data.posts.map((post) => {
+        return <div>{post.title}</div>;
+      })}
+    </>
+  );
 }
 
 export default App;
